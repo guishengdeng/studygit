f55b7dc2d67e185f3e24cb5e0fb7f0174c2898b1
guisheng.deng <1582340648@qq.com>
2017-04-12 10:40:40 +0800

    忽略了刚创建的项目的相关文件

---
+++ src/main/java/com/spring/jpa/repository/AccountResitory.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.repository;
+
+/**
+ * AccountResitory
+ *
+ * @author guisheng.deng
+ * @date 2017年04月12日
+ * @reviewer
+ * @description
+ * @see
+ */
+public interface AccountResitory {
+}
---
+++ src/main/java/com/spring/jpa/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.spring.jpa.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * AccountService
+ *
+ * @author guisheng.deng
+ * @date 2017年04月12日
+ * @reviewer
+ * @description
+ * @see
+ */
+@Service
+public class AccountService {
+}
