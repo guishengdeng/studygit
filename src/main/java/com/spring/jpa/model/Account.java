427cab05f2eadc12d57be108c4793529de33c501
guisheng.deng <1582340648@qq.com>
2017-04-12 10:40:40 +0800

    忽略了刚创建的项目的相关文件

---
+++ src/main/java/com/spring/jpa/model/Account.java
@@ -0,0 +1,17 @@
+package com.spring.jpa.model;
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Account
+ *
+ * @author guisheng.deng
+ * @date 2017年04月12日
+ * @reviewer
+ * @description
+ * @see
+ */
+@Entity
+@Table(name = "account")
+public class Account implements Serializable {
+}
